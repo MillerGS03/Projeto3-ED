b0fa8567a23a920250cf24ef3435d0f6f11f34b5
Diego Henrique <diego258henrique@gmail.com>
2019-06-07 19:49:59 -0300

    Classe cidade e seus atributos

---
+++ apCaminhosMarte/Cidades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    class Cidades
+    {
+        private int idCidade, x, y;
+        private string nomeCidade;
+
+        public string NomeCidade { get => nomeCidade; set => nomeCidade = value; }
+        public int IdCidade { get => idCidade; set => idCidade = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public Cidades(int id, string nome, int coordenadaX, int coordenadaY)
+        {
+            this.idCidade = id;
+            this.x = coordenadaX;
+            this.y = coordenadaY;
+            this.nomeCidade = nome;
+        }
+    }
+}
